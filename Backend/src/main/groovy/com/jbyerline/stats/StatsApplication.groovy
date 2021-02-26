3f8baaeafdf3be5396134471b0642af79e312c1a
Jacob Byerline <jbyerline@gmail.com>
2021-02-25 21:23:15 -0800

    Initialized SpringBoot

---
+++ Backend/src/main/groovy/com/jbyerline/stats/StatsApplication.groovy
@@ -0,0 +1,13 @@
+package com.jbyerline.stats
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class StatsApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(StatsApplication, args)
+	}
+
+}
